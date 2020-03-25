0d55e11c30deb73af25afbab88df7b501d4ae6bb
Ry Sutton <ryancesutton@gmail.com>
2020-03-25 10:50:47 -0500

    complete break and continue exercise

---
+++ js/break_and_continue.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+
+for (var i = 1; i < 50; i++) {
+    var userInput = prompt('Enter an odd number please: ');
+    userInput = parseInt(userInput);
+    if (userInput % 2 !== 0) {
+        console.log('Number to skip is: ' + userInput);
+        break;
+    }
+}
+
+for (var i = 1; i < 50; i+=2) {
+    if (i === userInput) {
+        console.log('Yikes! Skipping number: ' + i);
+        continue;
+    }
+    console.log('Here is an odd number: ' + i);
+}
+
+
+
+
+
+
+
